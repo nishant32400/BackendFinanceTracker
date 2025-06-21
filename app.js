f30005f37a64846a9774213bdb68146546859b97
nishant32400 <67227861+nishant32400@users.noreply.github.com>
2025-06-21 14:46:29 +0530

    Update app.js
    cors

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ const budgetRoutes = require('./routes/budgets');
 const adminRoutes = require('./routes/admin');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'https://frontend-finance-tracker-git-master-nishants-projects-3489f8e5.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
